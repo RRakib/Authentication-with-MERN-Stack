47da09d33ea30d5586c93e55135248b90c85cec3
Rakib Uddin <16103030@iubat.edu>
2019-03-23 22:41:29 +0600

    Redux Store and rootReducer Setup complete

---
+++ client/src/Store/store.js
@@ -0,0 +1,15 @@
+import {createStore , applyMiddleware, compose} from "redux";
+import rootReducers from "./Reducers/rootReducers";
+import thunk from "redux-thunk"
+
+const middleware = [thunk]
+
+const store = createStore(
+                            rootReducers,
+                            compose(
+                                applyMiddleware(...middleware),
+                                window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+                            )
+                         )
+
+export default store
